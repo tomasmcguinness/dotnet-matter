90cac2190e1ba6a901de18d4e5df1251f59cec0e
Tomas McGuinness <tomas@tomasmcguinness.com>
2023-02-25 21:32:13 +0000

    Parse passcode and discriminator from manual pairing code.

--- Commissioner.App/MainWindow.xaml.cs
+++ Commissioner.App/MainWindow.xaml.cs
@@ -6,51 +6,39 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.Reflection.Metadata.Ecma335;
-using System.Reflection.PortableExecutable;
 using System.Runtime.InteropServices.WindowsRuntime;
-using System.Text;
 using Windows.Devices.Bluetooth;
 using Windows.Devices.Bluetooth.Advertisement;
 using Windows.Devices.Bluetooth.GenericAttributeProfile;
-using Windows.Media.Devices;
 using Windows.Storage.Streams;
-
-// To learn more about WinUI, the WinUI project structure,
-// and more about our project templates, see: http://aka.ms/winui-project-info.
 
 namespace Commissioner.App
 {
-    /// <summary>
-    /// An empty window that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class MainWindow : Window
     {
         private BluetoothLEAdvertisementWatcher _watcher;
+
+        private short _currentDiscriminator;
+        private int _currentSetupCode;
 
         public MainWindow()
         {
             this.InitializeComponent();
 
+            RootGrid.DataContext = new MainWindowViewModel();
+
             _watcher = new BluetoothLEAdvertisementWatcher();
             _watcher.Received += watcher_Received;
-
         }
 
         private List<ulong> _devices = new List<ulong>();
 
         private async void watcher_Received(BluetoothLEAdvertisementWatcher sender, BluetoothLEAdvertisementReceivedEventArgs args)
         {
-
             if (_devices.Contains(args.BluetoothAddress))
             {
                 return;
             }
-
-            Debug.WriteLine("===============================");
-
-            Debug.WriteLine(args.BluetoothAddress);
-            Debug.WriteLine(args.Advertisement.LocalName);
 
             _devices.Add(args.BluetoothAddress);
 
@@ -60,8 +48,6 @@ namespace Commissioner.App
                 //
                 if (section.DataType == 0x16)
                 {
-                    Debug.WriteLine("Service Data");
-
                     DataReader dataReader = DataReader.FromBuffer(section.Data);
                     byte[] bytes = new byte[section.Data.Length];
                     dataReader.ReadBytes(bytes);
@@ -72,6 +58,8 @@ namespace Commissioner.App
 
                     if (section.Data.Length == 10)
                     {
+                        _watcher.Stop();
+
                         var device = await BluetoothLEDevice.FromBluetoothAddressAsync(args.BluetoothAddress);
                         Debug.WriteLine($"BLEWATCHER Found: {device.Name}");
 
@@ -94,23 +82,40 @@ namespace Commissioner.App
                             Debug.WriteLine($"Characteristic UUID: {characteristic.Uuid}");
                         }
 
-                        
-                        byte handshake = 0x00;
+                        BitArray handShake = new BitArray(8);
+                        handShake[1] = true;
+                        handShake[2] = true;
+                        handShake[5] = true;
+                        handShake[7] = true;
 
-                        byte[] handshakeBytes = new byte[] { handshake };
+                        byte[] handShakeBytes = new byte[9];
 
-                        await btpCharacteristics.Characteristics[0].WriteValueAsync(handshakeBytes.AsBuffer());
+                        handShake.CopyTo(handShakeBytes, 0);
+
+                        handShakeBytes[1] = 0x6C;
+
+                        //BitArray version = new BitArray(8, false);
+                        //version[7] = true;
+
+                        //version.CopyTo(handShakeBytes, 2);
+
+                        handShakeBytes[8] = 244;
+
+                        await btpCharacteristics.Characteristics[0].WriteValueAsync(handShakeBytes.AsBuffer());
 
                         btpCharacteristics.Characteristics[1].ValueChanged += MainWindow_ValueChanged;
 
                         GattCommunicationStatus status = await btpCharacteristics.Characteristics[1].WriteClientCharacteristicConfigurationDescriptorAsync(GattClientCharacteristicConfigurationDescriptorValue.Indicate);
+
+                        if (status == GattCommunicationStatus.Success)
+                        {
+                            // We've done it!
+                        }
                     }
 
                     Debug.WriteLine(hexString);
                 }
             }
-
-            Debug.WriteLine("===============================");
         }
 
         private void MainWindow_ValueChanged(GattCharacteristic sender, GattValueChangedEventArgs args)
@@ -120,20 +125,63 @@ namespace Commissioner.App
 
         private void myButton_Click(object sender, RoutedEventArgs e)
         {
-            if (_watcher.Status == BluetoothLEAdvertisementWatcherStatus.Started)
-            {
-                _devices.Clear();
-                _watcher.Stop();
-            }
-            else
-            {
-                _watcher.Start();
-            }
-
-            //string manualPairingCode = "34970112332";
-
-            // Parse this to extract the setup code and discriminator
+            // Parse this to manual pairing code into parts
             //
+            var viewModel = (MainWindowViewModel)RootGrid.DataContext;
+
+            var digit1 = short.Parse(viewModel.ManualPairingCode.Substring(0, 1));
+            var digits2to6 = int.Parse(viewModel.ManualPairingCode.Substring(1, 5));
+            var digits7to10 = int.Parse(viewModel.ManualPairingCode.Substring(6, 4));
+
+            var firstNumberBits = new BitArray(new int[] { digit1 });
+            var secondNumberBits = new BitArray(new int[] { digits2to6 });
+            var thirdNumberBits = new BitArray(new int[] { digits7to10 });
+
+            var discriminatorBitArray = new BitArray(12, false);
+            discriminatorBitArray[11] = firstNumberBits[0];
+            discriminatorBitArray[10] = firstNumberBits[1];
+            discriminatorBitArray[9] = secondNumberBits[14];
+            discriminatorBitArray[8] = secondNumberBits[15];
+
+            var discriminatorBytes = new byte[2];
+
+            discriminatorBitArray.CopyTo(discriminatorBytes, 0);
+
+            _currentDiscriminator = BitConverter.ToInt16(discriminatorBytes, 0);
+            Debug.WriteLine($"Discriminator: {_currentDiscriminator}");
+
+            var setupCodeBitArray = new BitArray(27);
+
+            for (int i = 0; i < 14; i++)
+            {
+                setupCodeBitArray[i] = secondNumberBits[i];
+            }
+
+            for (int i = 0; i < 13; i++)
+            {
+                setupCodeBitArray[i + 14] = thirdNumberBits[i];
+            }
+
+            var setupCodeBytes = new byte[5];
+
+            setupCodeBitArray.CopyTo(setupCodeBytes, 0);
+
+            _currentSetupCode = BitConverter.ToInt32(setupCodeBytes, 0);
+
+            Debug.WriteLine($"Setup Code: {_currentSetupCode}");
+
+            // Start discovery looking for a device with the specified discriminator!
+            //
+
+            //if (_watcher.Status == BluetoothLEAdvertisementWatcherStatus.Started)
+            //{
+            //    _devices.Clear();
+            //    _watcher.Stop();
+            //}
+            //else
+            //{
+            //    _watcher.Start();
+            //}
         }
     }
 }
---
+++ Commissioner.App/MainWindowViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Commissioner.App
+{
+    internal class MainWindowViewModel
+    {
+        public string ManualPairingCode { get; set; } = "34970112332";
+    }
+}
