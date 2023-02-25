77d6ac84bc31d62f1e3f7f99da809ba314068644
Tomas McGuinness <tomas@tomasmcguinness.com>
2023-02-25 17:15:23 +0000

    Experiments in listening for advertisments

--- Commissioner.App/MainWindow.xaml.cs
+++ Commissioner.App/MainWindow.xaml.cs
@@ -2,7 +2,17 @@
 // Licensed under the MIT License.
 
 using Microsoft.UI.Xaml;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Reflection.Metadata.Ecma335;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using Windows.Devices.Bluetooth;
 using Windows.Devices.Bluetooth.Advertisement;
+using Windows.Media.Devices;
+using Windows.Storage.Streams;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -22,16 +32,98 @@ namespace Commissioner.App
 
             _watcher = new BluetoothLEAdvertisementWatcher();
             _watcher.Received += watcher_Received;
+
         }
 
-        private void watcher_Received(BluetoothLEAdvertisementWatcher sender, BluetoothLEAdvertisementReceivedEventArgs args)
+        private List<ulong> _devices = new List<ulong>();
+
+        private async void watcher_Received(BluetoothLEAdvertisementWatcher sender, BluetoothLEAdvertisementReceivedEventArgs args)
         {
-            
+
+            if (_devices.Contains(args.BluetoothAddress))
+            {
+                return;
+            }
+
+            Debug.WriteLine("===============================");
+
+            Debug.WriteLine(args.BluetoothAddress);
+            Debug.WriteLine(args.Advertisement.LocalName);
+
+            _devices.Add(args.BluetoothAddress);
+
+            foreach (var section in args.Advertisement.DataSections)
+            {
+                // Check the service data.
+                //
+                if (section.DataType == 0x16)
+                {
+                    Debug.WriteLine("Service Data");
+
+                    DataReader dataReader = DataReader.FromBuffer(section.Data);
+                    byte[] bytes = new byte[section.Data.Length];
+                    dataReader.ReadBytes(bytes);
+
+                    string hexString = BitConverter.ToString(bytes);
+
+                    Debug.WriteLine(section.Data.Length);
+
+                    if(section.Data.Length == 10)
+                    {
+                        var device = await BluetoothLEDevice.FromBluetoothAddressAsync(args.BluetoothAddress);
+                        Debug.WriteLine($"BLEWATCHER Found: {device.Name}");
+                    }
+
+                    Debug.WriteLine(hexString);
+                }
+            }
+
+            Debug.WriteLine("===============================");
+
+            // Just build up a list of devices. The payload should contain the discriminator we need. 
+            //
+            //    if(args.Advertisement.ManufacturerData?.Count > 0)
+            //    {
+            //        foreach(var data in args.Advertisement.ManufacturerData)
+            //        {
+            //            if (data.CompanyId == 65535 || data.CompanyId == 89)
+            //            {
+            //                if (data.Data.Length >= 10)
+            //                {
+            //                    Debug.WriteLine(data.CompanyId);
+
+            //                    DataReader dataReader = DataReader.FromBuffer(data.Data);
+            //                    byte[] bytes = new byte[data.Data.Length];
+            //                    dataReader.ReadBytes(bytes);
+
+            //                    string hexString = BitConverter.ToString(bytes);
+
+            //                    Debug.WriteLine(hexString);
+            //                    Debug.WriteLine(hexString.Replace('-', ' '));
+
+            //                    Debug.WriteLine("===============================");
+            //                }
+            //            }
+            //        }
+            //    }
         }
 
         private void myButton_Click(object sender, RoutedEventArgs e)
         {
-            _watcher.Start();
+            if (_watcher.Status == BluetoothLEAdvertisementWatcherStatus.Started)
+            {
+                _devices.Clear();
+                _watcher.Stop();
+            }
+            else
+            {
+                _watcher.Start();
+            }
+
+            //string manualPairingCode = "34970112332";
+
+            // Parse this to extract the setup code and discriminator
+            //
         }
     }
 }
--- README.md
+++ README.md
@@ -5,7 +5,7 @@ I'm not sure this can even be made to work, but I'd love to be able to perform s
 
 There are a few pieces to get working.
 
-#1 using Bluetooth to discover the device.
+#1 using Bluetooth to connect to the device for commissioning.
 
 #2 Connect to the device
 
@@ -15,4 +15,17 @@ There are a few pieces to get working.
 
 #5 Connect to the device
 
+## Notes
+
+### 25/02 
+
+The first step is decoding the commissioning code which contains the information about the device. If we look at the Nordic Light Switch sample (https://developer.nordicsemi.com/nRF_Connect_SDK/doc/2.2.99-dev3/nrf/samples/matter/light_switch/README.html), you'll see they provide onboarding information. This is in the form of a QR code. It contains 
+information about the device, including it's setup code. 
+
+The first step for my commissioner is to take the manual pairing code, parse it and then use Bluetooth to find the device.
+
+I downloaded the Matter 1.0 Core specification and, in Chapter 5, it runs through the Onboarding Payload.
+
+08:52
+First new thing for me - What the heck is Base38 encoding??
 
