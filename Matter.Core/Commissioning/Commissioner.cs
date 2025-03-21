da552c737acb83a5fd8f4350a4373c2c19484aee
Tomas McGuinness <tomas@tomasmcguinness.com>
2025-03-21 07:18:15 +0000

    Clean up some of the code.

--- Matter.Core/Commissioning/Commissioner.cs
+++ Matter.Core/Commissioning/Commissioner.cs
@@ -1,5 +1,4 @@
 ﻿using Windows.Devices.Bluetooth.Advertisement;
-using Windows.Devices.Enumeration;
 using Windows.Storage.Streams;
 
 namespace Matter.Core.Commissioning
@@ -8,6 +7,9 @@ namespace Matter.Core.Commissioning
     {
         public void StartBluetoothDiscovery()
         {
+            // TODO Abstract the Bluetooth code behind an interface so we can use different providers
+            // e.g. Linux. Using the BluetoothLEAdvertisementWatcher ties us to Windows.
+            //
             BluetoothLEAdvertisementWatcher bluetoothLEAdvertisementWatcher = new();
             bluetoothLEAdvertisementWatcher.AllowExtendedAdvertisements = true;
             bluetoothLEAdvertisementWatcher.ScanningMode = BluetoothLEScanningMode.Active;
@@ -25,51 +27,17 @@ namespace Matter.Core.Commissioning
                     reader.ReadBytes(data);
                 }
 
-                Console.WriteLine(string.Format("{0}|0x{1:X}|0x{2:X}", section.Data.Length, section.DataType, BitConverter.ToString(data)));
+                // Check the payload. If we find 0xFFF6 we have a Matter advertising packet.
+                //
+                if (section.DataType == 0x16 && data[0] == 0xF6 && data[1] == 0xFF)
+                {
+                    // Check the discriminator (it's in bytes 3 and 4)
+                    //
+                    var disriminator = BitConverter.ToUInt16(data, 3);
+
+                    Console.WriteLine("Matter device payload received from {0} [{1}] with a disriminator of {2}", args.BluetoothAddress, args.BluetoothAddressType, disriminator);
+                }
             }
-
-            //0x1 0x06 0x16 0xF6-FF-00-00-0F-F1-FF-00-80-00
-
-            Console.Write("\n");
-        }
-
-        private void OnDeviceAdded(DeviceWatcher sender, DeviceInformation args)
-        {
-            Console.WriteLine($"Found: {args.Name}");
-        }
-
-        public async Task CommissionNode()
-        {
-            string aqs = "System.Devices.Aep.ProtocolId:=\"{bb7bb05e-5972-42b5-94fc-76eaa7084d49}\""; // Bluetooth LE Protocol ID
-
-            //DeviceInformationCollection collection = await DeviceInformation.FindAllAsync(aqs);
-
-            //foreach (DeviceInformation device in collection)
-            //{
-            //    Console.WriteLine(device.Name);
-            //}
-
-
-
-            //var discoveredDevices = await Bluetooth.ScanForDevicesAsync();
-            //Console.WriteLine($"found {discoveredDevices?.Count} devices");
-            //var bluetoothClient = new BluetoothClient();
-            //var watcher = new BluetoothLEAdvertisementWatcher();
-
-            //SerialPort BlueToothConnection = new SerialPort();
-
-            //BlueToothConnection.BaudRate = (9600);
-
-            //BlueToothConnection.PortName = "COM4";
-
-            //BlueToothConnection.Open();
-
-            //var hasBluetooth = await Bluetooth.GetAvailabilityAsync();
-
-            //if (!hasBluetooth)
-            //{
-            //    throw new Exception("No Bluetooth");
-            //}
         }
     }
 }
