09bf9ee993e0b2211d93c89125cd307a1cb96bd9
Tomas McGuinness <tomas@tomasmcguinness.com>
2025-04-28 14:37:28 +0100

    Replace Tailwind with Bootstrap for licensing reasons.

---
+++ Matter.WebController/Controllers/DevicesApiController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Matter.WebController.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DevicesApiController : ControllerBase
+    {
+        public IActionResult Post()
+        {
+
+            return Ok();
+        }
+    }
+}
---
+++ Matter.WebController/Controllers/DevicesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Matter.WebController.Controllers
+{
+    public class DevicesController : Controller
+    {
+        public IActionResult Add()
+        {
+            return View();
+        }
+    }
+}
